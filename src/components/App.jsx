a0c89ef90a8c69b839bc19e5f4f01007b413af6a
Fernando Gimenez <fer@MacBook-Air-de-Fernando.local>
2022-11-24 12:05:02 +0100

    2nd commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Cat from "../assets/images/cat.png";
 
 const App = () => {
   return (
     <div>
       <h1>Hello world, cambia?? !</h1>
+      <img src={Cat} alt="cat" />
     </div>
   );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,20 @@ module.exports = {
         test: /\.(css|scss|sass)$/,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/images/[name].[hash].[ext]",
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/fonts/[name].[hash].[ext]",
+        },
+      },
     ],
   },
   plugins: [
